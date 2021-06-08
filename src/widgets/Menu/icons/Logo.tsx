2a340705c5a6f1c4d8fc7e045ab833ab143e8e5c
josiahcuenza <josiahcuenza@gmail.com>
2021-06-08 18:26:14 +0800

    [update] - added images.js to utils

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Svg from "../../../components/Svg/Svg";
 import { SvgProps } from "../../../components/Svg/types";
-
+import images from '../../../util/images';
 interface LogoProps extends SvgProps {
   isDark: boolean;
 }
 
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
-  const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 205 26" {...props}>
-      <image width="205" height="26" href={isDark ? '/images/egg/LogoTextNewDark.png' : '/images/egg/LogoTextNewWhite.png'}/>
+      <image width="205" height="26" href={isDark ? images.logoTextNewDark.src : images.logoTextNewWhite.src}/>
     </Svg>
   )
 };
